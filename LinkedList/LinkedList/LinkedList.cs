04a5815097f80f5299992a4375769b3d30be15d6
pappurathod <prathod4545@gmail.com>
2022-03-23 20:48:05 +0530

    Add Node to linked list

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -16,5 +16,41 @@ namespace LinkedList
             Head = null;
             Tail = null;
         }
+
+        public void AddNode(Node node)
+        {
+            if (Head == null && Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                node.next = Head;
+                Head = node;
+            }
+        }
+        public void Display()
+        {
+            Node temp = Head;
+            if (temp == null)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine("linked list as follows");
+
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data);
+                if (temp.next != null)
+                {
+                    Console.Write(" ->");
+                }
+                temp = temp.next;
+            }
+        }
     }
 }
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -9,6 +9,12 @@ namespace LinkedList
             Node node1 = new Node(22);
             Node node2 = new Node(23);
             Node node3 = new Node(24);
+
+            linkedList.AddNode(node1);
+            linkedList.AddNode(node2);
+            linkedList.AddNode(node3);
+
+            linkedList.Display();
         }
     }
 }
