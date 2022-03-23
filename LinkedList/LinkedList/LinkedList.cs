04b1243462af410e7459debec524ebc7b3e538d5
pappurathod <prathod4545@gmail.com>
2022-03-23 20:39:46 +0530

    Create Linked List

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedList
+    {
+        public Node Head;
+        public Node Tail;
+
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+        }
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LinkedList
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            Node node1 = new Node(22);
+            Node node2 = new Node(23);
+            Node node3 = new Node(24);
+        }
+    }
+}
